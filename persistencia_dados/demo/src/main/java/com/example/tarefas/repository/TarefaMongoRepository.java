0be224d6f9a89d66fd6f02999e34914026782b9a
David Mendes <pciabela@gmail.com>
2025-04-11 14:13:30 -0300

    Delete persistencia_dados/demo directory

--- persistencia_dados/demo/src/main/java/com/example/tarefas/repository/TarefaMongoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.exemplo.tarefas.repository;
-
-import com.exemplo.tarefas.model.Tarefa;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TarefaMongoRepository extends MongoRepository<Tarefa, String> {}
--- persistencia_dados/demo/src/main/java/com/example/tarefas/repository/TarefaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.exemplo.tarefas.repository;
-
-import com.exemplo.tarefas.model.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {}
